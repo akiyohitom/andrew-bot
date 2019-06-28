7e5e2e303e86f7797846319258aaa6b408d2e2c4
Andrew <a.kiyohito.m@outlook.com>
2019-06-27 20:49:35 -0700

    Fixed unicode bug and emojis from other servers.

--- bot.js
+++ bot.js
@@ -1,3 +1,10 @@
+// Andrew
+/* 
+TODO
+- Print multiple images (parse and print all in same image)
+- Separate into files eventually
+*/
+
 // Declarations
 const Discord 	= require ("discord.js");
 const config 	= require('./config.json');    
@@ -5,9 +12,20 @@ const https     = require('https');
 const fs        = require('fs');                // FileSystem
 const client 	= new Discord.Client();         // Server
 
+var testChannel = null;
+
 // Startup
 client.on("ready", () => {
     console.log("Bot has initiated.");
+
+    console.log("List of channels:");
+    client.channels.forEach((item, index) => { 
+        console.log(item.id + " " + item.name);
+        if(item.name == "test-channel")
+        {
+            testChannel = item;
+        }
+    });
 });
 
 // Listening to client messages
@@ -15,10 +33,15 @@ client.on("ready", () => {
 //     message.author.id
 client.on("message", (message) => {
 	// Check if we're missing our prefix
-	if(message.content.indexOf(config.prefix) === 0 || message.author.bot)
-	{
-	    var args = message.content.slice(config.prefix.length).trim().split(/ +/g);
-    	var command = args.shift().toLowerCase();
+    var sender = message.author.username;
+    if(!message.author.bot) console.log(sender + " has sent message: ( " + message + " )");
+
+	if(message.content.indexOf(config.prefix) === 0 || message.content.indexOf(config.testprefix) === 0 && !message.author.bot)
+	{  
+        var testflag = (message.content.charAt(0) == config.testprefix);
+	    var args     = message.content.slice(config.prefix.length).trim().split(/ +/g);
+    	var command  = args.shift().toLowerCase();
+        var test     = config.testprefix;
 
     	if(command === 'ping')
     	{
@@ -34,32 +57,59 @@ client.on("message", (message) => {
     		 
     		if(args[0])
     		{
+                try {
+        			let image = args[0];
+                    console.log("EMOJI: " + image);
+                    var type = image.charAt(1);
 
-    			let image = args[0];
-    			console.log("image: " + image);
 
-    			var imagecode = image.match( /:(\d+)>/igu)[0];
-                var imagecode = imagecode.substring(1, imagecode.length-1);
-    			console.log("image: " + imagecode);
+                    if(!_containsUnicode(image))
+                    {  
+                        var imagecode = image.match( /:(\d+)>/igu)[0];
+                        var imagecode = imagecode.substring(1, imagecode.length-1);
+                        
+                        console.log("IMAGE-CODE: " + imagecode);
 
-                var emoji = client.emojis.get(imagecode);
-                console.log("emoji: " + emoji.url);
+                        var filetype = ".png";
 
-                // Get filetype
-                var filetype = emoji.url.match(/\.[a-z]*$/igu)[0];
-                console.log("filetype: " + filetype);
+                        var emoji = client.emojis.get(imagecode);
 
-    			var filename = imagecode + filetype;
-    			var imagelink = emoji.url;//config.imagelink + filename;
+                        if(emoji != null)
+                        {
+                            console.log("EMOJI-URL: " + emoji.url);
+                            filetype      = emoji.url.match(/\.[a-z]*$/igu)[0];
+                            
+                            var filename  = imagecode + filetype;
+                            var imagelink = emoji.url;
+                        } else {
+                            if(type == 'a') 
+                                filetype = ".gif";
 
-                console.log("fulllink: " + imagelink);
+                            var filename  = imagecode + filetype;
+                            var imagelink = config.imagelink + filename;
+                        }
 
-                _download(imagelink, './image' + filetype, function() {
-                    message.channel.send({files: ['./image' + filetype]});
-                });
-    			 
+                        _download(imagelink, './image' + filetype, function() {       
+                            if(testflag && testChannel != null) 
+                            {
+                                _sendToTest({files: ['./image' + filetype]});
+                            } else {
+                                message.channel.send({files: ['./image' + filetype]});
+                            }
+                        });
+                    } else {
+                        message.channel.send(image); // image is unicode
+                    }
+                } catch (err) {
+                    console.log("An error has occured: REASON (" + err + ").");    
+                }   
     		} else {
-    			message.reply("Missing image parameter.");	
+                if(testflag && testChannel != null)
+                {
+                    _sendToTest("Error( Missing image parameter )");
+                } else {
+                    message.reply("Missing image parameter.");  
+                }
     		}
     	}	
 	} else return;
@@ -68,6 +118,10 @@ client.on("message", (message) => {
 client.login(config.token);
 
 // helper functions
+function _containsUnicode(s) {
+    return /[^\u0000-\u00ff]/.test(s);
+}
+
 function _download(url, dest, callback) {
     var file = fs.createWriteStream(dest)
     https.get(url, function(response) {
@@ -88,3 +142,7 @@ function _unicodeToChar(text) {
           });
 };
 
+function _sendToTest(text) {
+    console.log("Sent to test channel.");
+    testChannel.send(text);
+};
